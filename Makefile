9b1ec5c16befca77c85998cb3c631cf9836476f0
leo-andral <pro.leoandral@gmail.com>
2026-05-27 18:23:32 +0200

    v1 release : - Added a barely working parser to use "generation_carte" in a CLI - Fixed Makefile not properly building "bruit"

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 .PHONY : build run clean clean_obj clean_exec
-.DEFAULT_GOAL := run
+.DEFAULT_GOAL := build
 
 # Variables
-CFLAGS = -lm -Wall -fsanitize=address
+CC = gcc
+params = -Wall -fsanitize=address
 libraries = image.o math2.o gen_DC.o
 objects = $(libraries) bruit.o generation_carte.o
 executables = bruit generation_carte
@@ -15,10 +16,10 @@ gen_DC.o : gen_DC.h math2.h
 generation_carte.o : image.h gen_DC.h math2.h
 
 bruit: bruit.o $(libraries)
-	gcc -o bruit $(CFLAGS) bruit.o $(libraries) 
+	$(CC) -o bruit $(params) bruit.o $(libraries) -lm
 
 generation_carte : generation_carte.o $(libraries)
-	gcc -o generation_carte $(CFLAGS) generation_carte.o $(libraries)
+	$(CC) -o generation_carte $(params) generation_carte.o $(libraries) -lm
 
 build : $(executables)
 
--- generation_carte.c
+++ generation_carte.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "image.h"
 #include "math2.h"
@@ -20,19 +21,56 @@ Image* matrice_to_image(Matrice* mat) {
     return img;
 }
 
+void print_cli_help() {
+    printf("Utilisation : generation_carte TAILLE FICHIER\n");
+    printf("         ou : generation_carte TAILLE FICHIER ROUGHNESS SEED\n");
+    printf("Generer une heightmap carré de taille (2^TAILLE)+1 pixels de large\n");
+    printf("\n");
+    printf("Options disponibles :\n");
+    printf("    EXAMPLE (= valeur par défaut) : explication\n");
+    printf("\n");
+    printf("    ROUGHNESS (= 1.0) : float entre 0. et 1. définissant \"l'aléatoirité\" du terrain généré\n");
+    printf("    SEED (= aléatoire) : entier étant la graine d'aléatoire utilisé\n");
+    printf("    FICHIER (= nom du fichier) : le nom du fichier de sortie\n");
+}
 
-int main() {
-    Matrice* test = creer_matrice(1);
-    test->tab[0][1] = 0.5;
-    detruit_matrice(test);
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        fprintf(stderr, "generation_carte : parametres manquants pour la generation\n");
+        print_cli_help();
+        return 22;
+    }
+    else {
+        int n = atoi(argv[1]);
+        assert(n > 0);
+        Matrice* terrain = creer_matrice(n);
 
-    random_setseed(67);
+        if (argc == 3) {
+            random_randomseed();
+            generation_DC(terrain, 1);
+        } else if (argc == 5) {
+            int seed = atoi(argv[4]);
+            random_setseed(seed);
+            double h = atof(argv[3]);
+            generation_DC(terrain, h);        
+        } else {
+            fprintf(stderr, "generation_carte : nombre incorrect de parametres (%d)\n", argc-1);
+            detruit_matrice(terrain);
+            return 22;
+        }
+        
+        Image* heightmap = matrice_to_image(terrain);
+        detruit_matrice(terrain);
+        bool successful_export = exporterImage(heightmap, argv[2]);
 
-    Matrice* test_gen = creer_matrice(6);
-    generation_DC(test_gen, 0.75);
-    Image* img = matrice_to_image(test_gen);
-    bool successful_export = exporterImage(img, "testResults/test.pgm");
-    assert(successful_export);
-    detruireImage(img);
-    detruit_matrice(test_gen);
+        if (!successful_export) {
+            fprintf(stderr, "generation_carte : ecriture du fichier %s impossible\n", argv[2]);
+            detruireImage(heightmap);
+            return 9;
+        } else {
+            detruireImage(heightmap);
+            return 0;
+        }
+            
+    }
 }
